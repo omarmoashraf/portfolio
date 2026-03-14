f15bd7dc884c54dc88864b302fdfee6eff35f7fa
omarmoashraf <omarmoashraf05@gmail.com>
2026-03-14 16:03:30 +0200

    commit

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -35,7 +35,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/file/d/1BKH_XIC2x_HeCrkS0q1t-Shf1-_kcqy4/view"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 Download CV
